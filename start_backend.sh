2105374c731334144c034f2eb23afa9def5c3b01
master <devdjangoreact@gmail.com>
2022-12-29 22:21:23 +0200

    asdfsdfafdss

--- start_backend.sh
+++ start_backend.sh
@@ -2,4 +2,4 @@
 echo "Start backend ..."
 source "/home/dev/project/typeVersion/venv/bin/activate"
 cd "/home/dev/project/typeVersion/backend"
-python3 manage.py runserver
+python3 manage.py runserver 0.0.0.0:8000
